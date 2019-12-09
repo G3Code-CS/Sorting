df7fed8a5f423b6d5aee614458dc168bc96e1b70
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-12-09 12:07:09 -0600

    Completed binary-search: recursive - All stretch completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -40,7 +40,21 @@ def binary_search(arr, target):
 def binary_search_recursive(arr, target, low, high):
 
     middle = (low+high)//2
-
+    # print(middle, target, high, low, arr[middle])
     if len(arr) == 0:
         return -1  # array empty
+    if (arr[middle] == target):
+        # print(f'Target is equal to middle {target} = {arr[middle]} {middle}')
+        return middle
     # TO-DO: add missing if/else statements, recursive calls
+    if (target < arr[middle]):
+        return(binary_search_recursive(arr, target, low, middle))
+    elif (target > arr[middle]):
+        return(binary_search_recursive(arr, target, middle, high))
+    # elif (target == arr[middle]):
+    #     return middle
+
+
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+# print(binary_search_recursive(arr1, 0, 0, len(arr1)-1))
